37ca09820bbbe86c06c8609f1933a7136d512556
Ted <ted@timelinelabs.cn>
2022-05-13 10:56:44 +0800

    初始化项目

---
+++ zhi-blog-article/zhi-blog-article-boot/src/main/java/com/zhi/blog/article/BlogArticleApplication.java
@@ -0,0 +1,15 @@
+package com.zhi.blog.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ted
+ * @date 2022/5/12
+ **/
+@SpringBootApplication
+public class BlogArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogArticleApplication.class, args);
+    }
+}
