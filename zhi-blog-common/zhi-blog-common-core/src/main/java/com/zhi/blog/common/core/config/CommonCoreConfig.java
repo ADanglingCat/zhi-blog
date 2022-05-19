545fc56823c95a23bfa09b09860c7fa98bf51c8a
Ted <ted@timelinelabs.cn>
2022-05-19 14:11:04 +0800

    common: 增加nacos配置中心和注册中心

--- zhi-blog-common/zhi-blog-common-core/src/main/java/com/zhi/blog/common/core/config/CommonCoreConfig.java
+++ zhi-blog-common/zhi-blog-common-core/src/main/java/com/zhi/blog/common/core/config/CommonCoreConfig.java
@@ -1,5 +1,6 @@
 package com.zhi.blog.common.core.config;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Ted
  * @date 2022/5/13
  **/
+@EnableDiscoveryClient
 @Configuration
 @ComponentScan({"com.zhi.blog.common.core"})
 public class CommonCoreConfig {
